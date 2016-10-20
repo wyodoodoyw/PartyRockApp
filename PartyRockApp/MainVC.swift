a02d092f415a8ef1474e3b2cd2f5d2325aff2d90
wyodoodoyw <mwood051@gmail.com>
2016-10-20 16:22:59 -0400

    first commit

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Matthew Wood on 2016-10-20.
+//  Copyright © 2016 Matthew. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
